9db63972d4ade025881231362747f01e89b5d4f2
wuhuaji <wuhuaji0@gmail.com>
2021-04-28 11:36:04 +0800

    init

---
+++ src/main/java/Recv.java
@@ -0,0 +1,27 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Recv {
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false,null);
+        System.out.println("waiting for message, To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (comsumerTag,delivery) ->{
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println("[x] Received " + message );
+        };
+
+        channel.basicConsume(QUEUE_NAME,true, deliverCallback, consumerTag -> {});
+    }
+}
---
+++ src/main/java/Send.java
@@ -0,0 +1,31 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Send {
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+            String message = "Hello World, 你好世界";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+
+            System.out.println(" [x] Sent '" + message + "'");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
